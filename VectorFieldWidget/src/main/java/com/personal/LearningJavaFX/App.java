c11c98b36c10b75cdb383f86b33af4e039324a78
Vladimir <valdemar93@rambler.ru>
2020-07-21 14:50:37 +0200

    added plot axes box

--- VectorFieldWidget/src/main/java/com/personal/LearningJavaFX/App.java
+++ VectorFieldWidget/src/main/java/com/personal/LearningJavaFX/App.java
@@ -3,6 +3,7 @@ package com.personal.LearningJavaFX;
 import javafx.application.*;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -18,7 +19,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         Pane root = new Pane();
-        Arrow arrow = new Arrow();
+        /*Arrow arrow = new Arrow();
         root.getChildren().add(arrow);
 
         root.setOnMouseClicked(evt -> {
@@ -34,10 +35,17 @@ public class App extends Application {
                     arrow.setStartY(evt.getY());
                     break;
             }
-        });
-
+        });*/
+        VectorfieldWidget VectorField = new VectorfieldWidget(root, 400.0, 400.0);
         Scene scene = new Scene(root, 400, 400);
-        var SceneWidth = scene.getWidth();
+        ChangeListener<Number> stageSizeListener = (observable, oldValue, newValue) ->{
+             System.out.println("Height: " + stage.getHeight() + " Width: " + stage.getWidth());
+             VectorField.SetWidth(stage.getWidth());
+             VectorField.SetHeight(stage.getHeight());
+             VectorField.PlotVectorField();
+        };
+        stage.widthProperty().addListener(stageSizeListener);
+        stage.heightProperty().addListener(stageSizeListener); 
         stage.setScene(scene);
         stage.show();
     }
--- VectorFieldWidget/src/main/java/com/personal/LearningJavaFX/VectorFieldWidget.java
+++ VectorFieldWidget/src/main/java/com/personal/LearningJavaFX/VectorFieldWidget.java
@@ -1,5 +1,7 @@
 package com.personal.LearningJavaFX;
 
+import java.util.List;
+
 import javafx.application.*;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.DoubleProperty;
@@ -10,31 +12,103 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
 import javafx.stage.*;
 
+
+
 class VectorfieldWidget
 {
     private Pane m_ParrentPane;
-    private int m_Panewidth;
-    private int m_PaneHeight;
+    private double m_Width;
+    private double m_Height;
+    private double m_PrevWidth;
+    private double m_PrevHeight;
     
-    private double MaxX;
-    private double MinX;
-    private double MaxY;
-    private double MinY;
+    private double m_MaxX;
+    private double m_MinX;
+    private double m_MaxY;
+    private double m_MinY;
     
-    private double MaxDX;
-    private double MinDX;
-    private double MaxDY;
-    private double MinDY;
+    private double m_MaxDX;
+    private double m_MinDX;
+    private double m_MaxDY;
+    private double m_MinDY;
 
-    private double MaxVecDX;
-    private double MinVecDX;
-    private double MaxVecDY;
-    private double MinVecDY;
+    private double m_MaxVecDX;
+    private double m_MinVecDX;
+    private double m_MaxVecDY;
+    private double m_MinVecDY;
 
-    private double VecDScale;
+    private double m_VecDScale;
 
-    public void VectorfieldWidget(Pane ParrentPane)
+    private int m_FontSize;
+
+    private String m_XAxisName;
+    private String m_YAxisName;
+
+    private List<Double[]> m_VectorFieldData;
+
+
+    private Group m_Canvas;
+    private Rectangle m_PlotAxesRectangle;
+    
+
+	public VectorfieldWidget(Pane ParrentPane, double Width, double Height)
     {
         m_ParrentPane = ParrentPane;
-    };
+        m_Width = Width;
+        m_Height = Height;
+
+        m_Canvas = new Group();
+
+        m_PlotAxesRectangle = new Rectangle(0.3*Width,0.3*Height,0.6*Width,0.6*Height);
+        m_PlotAxesRectangle.setStroke(Color.BLACK);
+        m_PlotAxesRectangle.setStrokeWidth(3);
+        m_PlotAxesRectangle.setFill(null);
+        m_Canvas.getChildren().add(m_PlotAxesRectangle);
+        ParrentPane.getChildren().add(m_Canvas);
+    }
+
+    public void SetFontSize(int FontSize)
+    {
+        m_FontSize = FontSize;
+    }
+
+    public void SetScaleFactor(double ScaleFactor)
+    {
+        m_VecDScale = ScaleFactor;
+    }
+
+    public void SetXAxisName(String XAxisName)
+    {
+        m_XAxisName = XAxisName;
+    }
+
+    public void SetYAxisName(String YAxisName)
+    {
+        m_YAxisName = YAxisName;
+    }
+
+    public void SetVectorFieldData(List<Double[]> VectorFieldData)
+    {
+        m_VectorFieldData = VectorFieldData;
+    } 
+
+    public void SetWidth(double Width)
+    {
+        m_PrevWidth = m_Width;
+        m_Width = Width;
+    }
+
+    public void SetHeight(double Height)
+    {
+        m_PrevHeight = m_Height;
+        m_Height = Height;
+    }
+
+    public void PlotVectorField()
+    {
+        m_PlotAxesRectangle.setWidth(0.6*m_Width);  
+        m_PlotAxesRectangle.setHeight(0.6*m_Height);
+        m_PlotAxesRectangle.relocate(0.2*m_Width, 0.2*m_Height);
+        
+    }
 }
